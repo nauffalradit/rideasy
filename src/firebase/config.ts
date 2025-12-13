6c3e0dcb986cd3b4330d70a1aadd5e890e5efda5
M Nauffal Raditya <nauffalraditya93@gmail.com>
2025-12-13 11:32:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-256589824-f532c",
+  "appId": "1:739713034252:web:596147aea07589e90e6629",
+  "apiKey": "AIzaSyDuHKdcoEInIbzNdUBnse3czj9xgVx6lkg",
+  "authDomain": "studio-256589824-f532c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "739713034252"
+};
